5d986f8078e526c8df3aa282240382709f998921
Matheus Santos <matheus4589silva@gmail.com>
2025-06-14 14:09:51 -0300

    Update transformacoes.py

--- Projeto Loja Venda/Python Còdigo Loja Venda/transformacoes.py
+++ Projeto Loja Venda/Python Còdigo Loja Venda/transformacoes.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def traduzir_e_ordenar_meses(Vendas_mes):
-    #Cria o dicionário de mapeamento dos meses (tradução)
+    # Cria o dicionário de mapeamento dos meses (tradução)
     meses_em_portugues = {
         'January': 'Janeiro',
         'February': 'Fevereiro',
@@ -17,20 +17,21 @@ def traduzir_e_ordenar_meses(Vendas_mes):
         'December': 'Dezembro'
     }
 
-    if 'Mês' in Vendas_mes.columns:
-        Vendas_mes['Mês'] = Vendas_mes['Mês'].map(meses_em_portugues)
-    else:
-        print("Aviso: Coluna 'Mês' não encontrada no DataFrame para tradução.")
+    if 'Mês' not in Vendas_mes.columns:
+        print("Aviso: Coluna 'Mês' não encontrada no DataFrame.")
         return Vendas_mes
 
-    #Ordena os meses cronologicamente
+    # Traduz os meses de inglês para português
+    Vendas_mes['Mês'] = Vendas_mes['Mês'].map(meses_em_portugues).fillna(Vendas_mes['Mês'])
+
+    # Ordena os meses cronologicamente
     ordem_meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho',
                 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
     
-    #Trata os caso onde alguns meses podem não existir no DataFrame
+    # Trata os casos onde alguns meses podem não existir no DataFrame
     Vendas_mes['Mês'] = pd.Categorical(Vendas_mes['Mês'], categories=ordem_meses, ordered=True)
 
-    # Ordena o DataFrame pela nova coluna categórica 'Mes'
+    # Ordena o DataFrame pela nova coluna categórica 'Mês'
     Vendas_mes = Vendas_mes.sort_values('Mês')
 
     return Vendas_mes
